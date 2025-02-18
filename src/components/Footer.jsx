97bd04988f4a9b64c60f2abf7166bcb107917510
Ahmadraza <razabrothers360@gmail.com>
2025-02-19 04:03:59 +0500

    Updated file with new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ import { assets } from '../assets/assets'
 const Footer = () => {
     return (
         <div>
-            <div class="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+            <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
                 <div>
-                    <img src={assets.logo} class="mb-5 w-32" alt="" />
-                    <p class="w-full md:w-2/3 text-gray-600">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <img src={assets.logo} className="mb-5 w-32" alt="" />
+                    <p className="w-full md:w-2/3 text-gray-600">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
                 <div>
-                    <p class="text-xl font-medium mb-5">COMPANY</p>
-                    <ul class="flex flex-col gap-1 text-gray-600">
+                    <p className="text-xl font-medium mb-5">COMPANY</p>
+                    <ul className="flex flex-col gap-1 text-gray-600">
                         <li>Home</li>
                         <li>About us</li>
                         <li>Delivery</li>
@@ -19,16 +19,16 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div>
-                    <p class="text-xl font-medium mb-5">GET IN TOUCH</p>
-                    <ul class="flex flex-col gap-1 text-gray-600">
+                    <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+                    <ul className="flex flex-col gap-1 text-gray-600">
                         <li>+1-000-000-0000</li><li>greatstackdev@gmail.com</li>
-                        <li class="cursor-pointer">Instagram</li>
+                        <li className="cursor-pointer">Instagram</li>
                     </ul>
                 </div>
             </div>
             <div>
                 {/* <hr className='fill-white'/> */}
-                <p class="py-5 text-sm text-center border-t-1 border-gray-300">Copyright 2024@ forever.com - All Right Reserved.</p>
+                <p className="py-5 text-sm text-center border-t-1 border-gray-300">Copyright 2024@ forever.com - All Right Reserved.</p>
             </div>
         </div>
 
